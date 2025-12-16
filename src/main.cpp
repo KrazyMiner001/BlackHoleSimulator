46bc3cda28cf81d1bd09493cd98e7e78caaac6f7
KrazyMiner001 <krazyminer001@outlook.com>
2025-12-16 11:29:24 +0200

    feat: Basic window rendering

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,101 @@
-﻿int main() {
+﻿#include <glbinding/glbinding.h>
+#include <glbinding/gl/functions.h>
 
+#include <SDL3/SDL.h>
+
+#include <imgui.h>
+#include <imgui_impl_opengl3.h>
+#include <imgui_impl_sdl3.h>
+#include <iostream>
+
+
+#include <glm/glm.hpp>
+
+#include "glbinding/gl/bitfield.h"
+
+using namespace gl;
+using namespace glm;
+
+int main() {
+    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMEPAD)) {
+        std::cout << "SDL failed to initialise: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+
+    SDL_Window *window = SDL_CreateWindow(
+        "Black Hole Simulator",
+        600,
+        480,
+        SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE
+    );
+
+    if (window == nullptr) {
+        std::cout << "SDL failed to create window: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    const SDL_GLContext gl_context = SDL_GL_CreateContext(window);
+    if (gl_context == nullptr) {
+        std::cout << "SDL failed to create gl context: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    glbinding::initialize(SDL_GL_GetProcAddress, false);
+
+    SDL_GL_MakeCurrent(window, gl_context);
+    SDL_GL_SetSwapInterval(1);
+    SDL_SetWindowPosition(window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
+    SDL_ShowWindow(window);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    const ImGuiIO &io = ImGui::GetIO();
+    ImGui::StyleColorsDark();
+
+    ImGui_ImplSDL3_InitForOpenGL(window, gl_context);
+    ImGui_ImplOpenGL3_Init("#version 460");
+
+    SDL_Event event;
+    bool done = false;
+    while (!done) {
+        while (SDL_PollEvent(&event)) {
+            ImGui_ImplSDL3_ProcessEvent(&event);
+
+            switch (event.type) {
+                case SDL_EVENT_QUIT:
+                    done = true;
+                    break;
+            }
+        }
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplSDL3_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::Render();
+
+        glViewport(0, 0, io.DisplaySize.x, io.DisplaySize.y);
+        glClearColor(0.2, 0.3, 0.3, 1.0);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        SDL_GL_SwapWindow(window);
+    }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDL3_Shutdown();
+    ImGui::DestroyContext();
+
+    SDL_GL_DestroyContext(gl_context);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
